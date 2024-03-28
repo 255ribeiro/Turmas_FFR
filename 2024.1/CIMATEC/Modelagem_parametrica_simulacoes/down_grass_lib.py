af309674de490324e7900d6ea526e58000a4e133
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2024-03-28 00:14:29 -0300

    download grass lib

--- 2024.1/CIMATEC/Modelagem_parametrica_simulacoes/down_grass_lib.py
+++ 2024.1/CIMATEC/Modelagem_parametrica_simulacoes/down_grass_lib.py
@@ -1,4 +1,3 @@
-
 import zipfile, io
 
 def install_and_import(package):
@@ -14,6 +13,12 @@ def install_and_import(package):
 
 install_and_import('requests')
 
+Gh_folders = {
+"components_folder":"%AppData%\Grasshopper\Libraries",
+"settings_folder": "%AppData%\Grasshopper",
+"user_object_folder": "%AppData%\Grasshopper\UserObjects",
+"auto_save_folder": "%AppData%\Grasshopper\AutoSave" 
+}
 
 import requests, zipfile, io
 url = 'https://github.com/255ribeiro/intro_revit/raw/master/docs/revit/DADOS/projeto_revit/pastas_do_projeto.zip'
